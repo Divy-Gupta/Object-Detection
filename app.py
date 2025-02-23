404e2b8a79116c9c7fcfbd43d06865e1c226f819
Divy Gupta <154852793+Divy-Gupta@users.noreply.github.com>
2025-02-23 09:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+import streamlit as st
+
+
 from pathlib import Path
 import PIL.Image
-import streamlit as st
 import settings
 import helper
 import webbrowser
